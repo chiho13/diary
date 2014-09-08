906d0acdaa6cf5ec4c194c2ab274db583c5bea64
Anthony Ho <ahomnky11@gmail.com>
2014-09-08 17:08:29 +0100

    Initial Commit

---
+++ Diary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Diary
+//
+//  Created by Anthony Ho on 08/09/2014.
+//  Copyright (c) 2014 ZeroMondays. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZMAppDelegate class]));
+    }
+}
